74f181c263389769f17ebc96938a59b16a561aad
odmmry <799060974@qq.com>
2024-12-16 22:26:17 +0800

    1.3

--- QiJi/app/src/main/java/com/knight/asus_nb/knight/Util/CallBack.java
+++
@@ -1,7 +0,0 @@
-package com.knight.asus_nb.knight.Util;
-
-public interface CallBack {
-    public final static int SUCCESS = 0;
-    public final static int FAIL = 1;
-   void getResult(int Type);
-}
